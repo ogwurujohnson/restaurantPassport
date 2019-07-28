6021bcc7ef0538b2d959b6447c5315d905c52461
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-28 17:53:28 +0100

    Feat: Setup server entry point and create a dummy endpoint

--- server/api/api.js
+++ server/api/api.js
@@ -3,11 +3,22 @@ const helmet = require('helmet');
 const cors = require('cors');
 const logger = require('morgan');
 
+const authRoute = require('../auth/auth.route');
+
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(logger('dev'));
 app.use(cors());
+
+app.use('/api/v1/auth', authRoute);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to restaurant review api, kindly navigate to this link to explore the api /api/v1/ ',
+  });
+});
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
