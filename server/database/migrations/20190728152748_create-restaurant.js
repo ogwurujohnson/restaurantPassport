43ecaf2f017ffadd687ca76a270aa036fe244e62
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-28 16:07:03 +0100

    Chore: Create Migration files  - create restaurant migration file

---
+++ server/database/migrations/20190728152748_create-restaurant.js
@@ -0,0 +1,14 @@
+/* eslint-disable func-names */
+// eslint-disable-next-line no-unused-vars
+exports.up = function (knex) {
+  return knex.schema.createTable('restaurants', (tbl) => {
+    tbl.increments();
+    tbl.string('name', 255).notNullable();
+    tbl.string('description').nullable();
+    tbl.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('restaurants');
+};
