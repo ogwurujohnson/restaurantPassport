64eb984a588edbf80573f679025459d1e152c41a
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-28 18:09:59 +0100

    Chore: Setup auth model to include  - find,  - findBy,  - findById,  - createUser helper methods

---
+++ server/auth/auth.model.js
@@ -0,0 +1,39 @@
+/* eslint-disable consistent-return */
+const log = require('../utils').winston;
+const db = require('../database/dbConfig');
+
+const findBy = (filter) => {
+  try {
+    return db('users')
+      .where(filter)
+      .first();
+  } catch (err) {
+    log.info(err.message);
+  }
+};
+
+const findById = (id) => {
+  try {
+    return db('users')
+      .where({ id })
+      .first();
+  } catch (err) {
+    log.info(err.message);
+  }
+};
+
+const createUser = async (data) => {
+  try {
+    const [id] = await db('users').insert(data);
+    return findById(id);
+  } catch (err) {
+    log.info(err.message);
+  }
+};
+
+
+module.exports = {
+  findBy,
+  findById,
+  createUser,
+};
