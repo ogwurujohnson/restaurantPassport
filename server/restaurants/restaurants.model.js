f79b5ca3c71e5d7090e25a5d14f4fa5e32ce40ec
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-29 17:56:33 +0100

    Feat: Improve find restaurant endpoint to include avg ratings for the restaurant and number of reviews

--- server/restaurants/restaurants.model.js
+++ server/restaurants/restaurants.model.js
@@ -2,20 +2,41 @@
 const log = require('../utils').winston;
 const db = require('../database/dbConfig');
 
-
-const find = () => {
+const find = async () => {
   try {
-    return db('restaurants');
+    const restaurants = await db('restaurants AS r')
+      .select('r.id', 'r.name', 'r.description')
+      .count('rv.ratings AS no_of_reviews')
+      .sum('rv.ratings AS sum')
+      .leftJoin('reviews as rv', 'r.id', 'rv.restaurant_id')
+      .groupBy('r.id');
+    return restaurants.map(restaurant => ({
+      ...restaurant,
+      avgRating: parseInt(restaurant.sum / restaurant.no_of_reviews, 10),
+    }));
   } catch (err) {
     log.info(err.message);
   }
 };
 
-const findById = (id) => {
+const findRestaurantReviews = (id) => {
   try {
-    return db('restaurants')
+    return db('reviews AS r')
+      .select('r.id', 'u.firstname', 'u.lastname', 'r.ratings', 'r.reviews')
+      .join('users AS u', 'r.user_id', 'u.id')
+      .where({ restaurant_id: id });
+  } catch (err) {
+    log.info(err.message);
+  }
+};
+
+const findById = async (id) => {
+  try {
+    const restaurant = await db('restaurants')
       .where({ id })
       .first();
+    restaurant.reviews = await findRestaurantReviews(id);
+    return restaurant;
   } catch (err) {
     log.info(err.message);
   }
