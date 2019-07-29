c7dd1673443fbf824f7adbe90266c9f919100a10
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-29 07:33:30 +0100

    Feat: Create a users util file for refactoring repeating codes

---
+++ server/users/users.util.js
@@ -0,0 +1,19 @@
+const notFoundError = {
+  status: 404,
+  message: 'User(s) not found at this time, kindly check back again',
+};
+
+const customErrors = data => ({
+  message: `user ${data} successfully`,
+});
+
+const serverErrors = err => ({
+  message: err.message,
+  stack: err.stack,
+});
+
+module.exports = {
+  notFoundError,
+  customErrors,
+  serverErrors,
+};
