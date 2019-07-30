2029ada18bf98226837d9e11c64c6ab64216501a
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-30 12:11:18 +0100

    Feat: Create middleware validation for the users endpoint

--- server/users/users.route.js
+++ server/users/users.route.js
@@ -3,13 +3,14 @@ const {
   deleteUser, updateUser, getAll, getSingle,
 } = require('./users.controller');
 const { authenticate } = require('../auth/authenticate.util');
+const { validateUserId, validateUser } = require('./users.validate');
 
 const route = express.Router();
 
 route.get('/', getAll);
 
-route.get('/:id', getSingle);
-route.put('/:id', authenticate, updateUser);
-route.delete('/:id', authenticate, deleteUser);
+route.get('/:id', validateUserId, getSingle);
+route.put('/:id', authenticate, validateUser, validateUserId, updateUser);
+route.delete('/:id', authenticate, validateUserId, deleteUser);
 
 module.exports = route;
