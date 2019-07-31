4d94acb838cc8e6dc804cfcda49e23ff3c45c2f5
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-31 22:08:04 +0100

    Feat: Route the Restaurant and Passport endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './containers/Login';
 import Register from './containers/Register';
+import Restaurants from './containers/Restaurants';
+import Passport from './containers/Passport';
+import RestaurantDescription from './containers/RestaurantDescription';
 
 function App() {
   return (
     <Router>
       <div className="App">
-  <Route path='/' exact component={() => <div><p>Welcome</p></div>} />
+        <Route exact path='/' component={() => <div><p>Welcome</p></div>} />
         <Route path='/login' component={Login} />
         <Route path='/signup' component={Register} />
+        <Route exact path='/restaurants' component={Restaurants} />
+        <Route path='/restaurants/:id' component={RestaurantDescription} />
+        <Route exact path='/passport' component={Passport} />
       </div>
     </Router>
   );
