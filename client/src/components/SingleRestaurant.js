7a789d50d04bd8425cfc998cf6bf57ba427d9539
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-31 23:17:23 +0100

    Feat: Add links on each restaurant

--- client/src/components/SingleRestaurant.js
+++ client/src/components/SingleRestaurant.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SingleRestaurant(props) {
   return (
     <div>
-      <h1>Name: {props.restaurant.name}</h1>
+      <Link to={`/restaurants/${props.restaurant.id}`}><h1>Name: {props.restaurant.name}</h1></Link>
       <p>Description: {props.restaurant.description}</p>
     </div>
   )
