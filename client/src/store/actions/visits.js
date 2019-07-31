c39b2662c4fa33cb4e4c10cb3b00fadd5c0eca21
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-31 14:43:38 +0100

    Feat: Flesh out the visit action creators

---
+++ client/src/store/actions/visits.js
@@ -0,0 +1,31 @@
+import axiosPump from '../../utils/axios';
+
+export const addToVisits = (url) => dispatch => {
+  axiosPump().post(url)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+export const getVisits = (url) => dispatch => {
+  axiosPump().get(url)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+export const removeFromVisits = (url) => dispatch => {
+  axiosPump().get(url)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
