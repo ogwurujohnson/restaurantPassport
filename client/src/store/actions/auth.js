dbcc64f25749b3d140489f02b934e3140d92a6b9
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-31 15:49:40 +0100

    Feat: make use of the action creators to consume the authentication endpoints

--- client/src/store/actions/auth.js
+++ client/src/store/actions/auth.js
@@ -1,21 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
+import {
+  CREATE_USER,
+  CREATING_USER,
+  CREATE_USER_ERROR,
+  LOGIN_ERROR,
+  LOGIN_IN,
+  LOGIN_SUCCESS
+} from "./action.types";
 
 export const register = (url, credentials) => dispatch => {
-  axios.post(url, credentials)
-    .then((res) => {
+  dispatch({ type: CREATING_USER })
+  axios
+    .post(url, credentials)
+    .then(res => {
+      dispatch({ type: CREATE_USER, message: 'signup successful' })
       console.log(res);
     })
-    .catch((err) => {
+    .catch(err => {
+      dispatch({ type: CREATE_USER_ERROR, message: 'signup failed' })
       console.log(err);
-    })
-}
+    });
+};
 
 export const login = (url, credentials) => dispatch => {
-  axios.post(url, credentials)
-    .then((res) => {
+  dispatch({ type: LOGIN_IN })
+  axios
+    .post(url, credentials)
+    .then(res => {
+      dispatch({ type: LOGIN_SUCCESS, message: 'login successful', payload: res.data.token })
       console.log(res);
     })
-    .catch((err) => {
+    .catch(err => {
+      dispatch({ type: LOGIN_ERROR, message: 'login failed' })
       console.log(err);
-    })
-}
+    });
+};
