44a82f0efda196f3098ad79b462a9fb7030cfa72
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-01 07:34:13 +0100

    Feat: on the existence of a 401 error fire a NO_AUth warning and send the user out

--- client/src/middlewares/authentication.js
+++ client/src/middlewares/authentication.js
@@ -2,17 +2,18 @@ import {CREATE_USER, LOGIN_SUCCESS, NO_AUTH} from '../store/actions/action.types
 import {set, removeItem} from '../utils/localStorage.js';
 
 export const setToken = store => next => action => {
-    if(action.type === CREATE_USER || action.type === LOGIN_SUCCESS) {
-        set('token', action.payload);
-        set('user', JSON.stringify(action.userdata))
-    }
-    next(action);
+  if(action.type === CREATE_USER || action.type === LOGIN_SUCCESS) {
+      set('token', action.payload);
+      set('user', JSON.stringify(action.userdata))
+  }
+  next(action);
 }
 
 export const redirectAuth = store => next => action => {
-  if (action.type === NO_AUTH) {
-    removeItem("hashedToken");
-    window.location.reload();
-  }
-  next(action);
+if (action.type === NO_AUTH) {
+  removeItem("token");
+  removeItem('user');
+  window.location.reload();
+}
+next(action);
 };
