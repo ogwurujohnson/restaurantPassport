f423ca98364310d60254639b980c369234748e7b
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-01 12:02:18 +0100

    Feat: style the login  and registeration page

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../store/actions/auth';
 import {baseUrl} from '../utils/url';
+import LoginForm from '../components/forms/LoginForm';
+import styled from "styled-components";
+import orderfood from "../assets/bike rider.png";
+
 class Login extends Component {
   state = {
     email: '',
@@ -12,21 +16,32 @@ class Login extends Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+  handleSubmit = (data) => {
     const url = `${baseUrl}/auth/login`
-    this.props.login(url, this.state);
+    this.props.login(url, data);
   }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.email} onChange={this.handleChange}  name="email" placeholder="Email"  />
-          <input type="password" value={this.state.password} onChange={this.handleChange} name="password" placeholder="*****"  />
-          <button>Login</button>
-        </form>
-      </div>
+      <FormPageWrapper>
+        <LoginForm handleSubmit={this.handleSubmit} />
+        <DetailsWrapper>
+          <div className="intro">
+            <h4>Registeration is fast, easy, and free.</h4>
+            <p>Once you are registered you can: </p>
+          </div>
+          <div className="intro-image">
+            <img src={orderfood} alt="orderfood" />
+          </div>
+          <div className="contact">
+            <h4>Contact Customer Support</h4>
+            <p>
+              If you"re looking for more help or have a question to ask, please
+            </p>
+            <button>contact us</button>
+          </div>
+        </DetailsWrapper>
+      </FormPageWrapper>
     )
   }
 }
@@ -38,3 +53,61 @@ const mapStateToProps = store => {
 }
 
 export default connect(mapStateToProps, {login})(Login)
+
+const FormPageWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DetailsWrapper = styled.div`
+  width: 40%;
+  margin: 4rem 0rem;
+  margin-right: 10em;
+  .intro {
+    padding: 2rem;
+    border-bottom: 1px solid #25282b;
+    margin-bottom: 1rem;
+    h4 {
+      font-size: 2rem;
+      font-weight: 600;
+      font-family: "Source Sans Pro", sans-serif;
+      color: #25282b;
+      margin-bottom: 0.7rem;
+      line-height: 1.1;
+    }
+
+    p {
+      color: #25282b;
+    }
+  }
+
+  .intro-image {
+    width: 100%;
+    margin-bottom: 3rem;
+    img {
+      width: 100%;
+    }
+  }
+
+  .contact {
+    h4 {
+      font-size: 2rem;
+      font-weight: 600;
+      font-family: "Source Sans Pro", sans-serif;
+      color: #25282b;
+      margin-bottom: 0.7rem;
+      line-height: 1.1;
+    }
+
+    button {
+      width: 30%;
+      background-color: #f30;
+      color: #fff;
+      border: 1px solid #f30;
+      padding: 1.2rem;
+      font-size: 1.4rem;
+      font-weight: bold;
+      cursor: pointer;
+    }
+  }
+`;
