03026d5f47c503de8191680a542a11be733ed1e8
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-07 16:44:56 +0100

    remove devtools thingy

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk, setToken, logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
